6d815ed1ca71cebea6d80b34a7886a6123c913b1
adrien-glg <75178159+adrien-glg@users.noreply.github.com>
2023-01-09 23:04:04 +0100

    modify code only >> generic

--- src/aws/modify_code_only.sh
+++ src/aws/modify_code_only.sh
@@ -2,14 +2,14 @@
 
 # USE THIS SCRIPT TO MODIFY CODE ONLY (INSIDE ZIP PACKAGE), AFTER RUNNING ./make_lambda_package.sh AT LEAST ONCE
 
+LAMBDA_HANDLER="mobiledet_lambda.py"
+
 source config.sh
 
 unzip ${ZIP_PACKAGE_PATH} -d ${PACKAGE_PATH}/package
 
 cd ${PACKAGE_PATH}/package
-## WARNING: NOT GENERIC BELOW
-rm -r mobiledet_lambda.py src ${IMAGE_FILE}
-####
+rm -r ${LAMBDA_HANDLER} src ${IMAGE_FILE}
 cd -
 
 cp -r ${LAMBDA_CODE}/* ${INPUT_IMAGE_PATH} ${PACKAGE_PATH}/package
